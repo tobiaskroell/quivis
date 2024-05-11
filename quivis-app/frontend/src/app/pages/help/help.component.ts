695ac8583fc00d6a1d982f99d7145f525e07ac43
Tobias Kroell <kroelltob@gmail.com>
2024-05-11 19:58:38 +0200

    Add "help" component

--- quivis-app/frontend/src/app/pages/help/help.component.ts
+++ quivis-app/frontend/src/app/pages/help/help.component.ts
@@ -6,34 +6,50 @@ import { Component, OnInit } from '@angular/core';
 export class HelpComponent implements OnInit {
 
     items: any[] = [];
+    answers: any[] = [];
 
     activeIndex: number = 0;
 
     constructor() { }
 
     ngOnInit(): void {
-        this.items = [{ 
-                label: 'General', icon: 'pi pi-fw pi-info-circle', questions: [
-                    'Is there a trial period?', 'Do I need to sign up with credit card?', 'Is the subscription monthly or annual?', 'How many tiers are there?'
+        this.items = [
+            { 
+                label: 'Bedienung', 
+                icon: 'pi pi-fw pi-info-circle', 
+                questions: [
+                    'Dashboard', 
+                    'Medizinische Historie', 
+                    'Medikationsplan', 
+                    'Frag Claude'
                 ] 
             },
+            
             { 
-                label: 'Mailing', icon: 'pi pi-fw pi-envelope', questions: [
-                    'How do I setup my account?', 'Is there a limit on mails to send?', 'What is my inbox size?', 'How can I add attachements?'
+                label: 'Häufig gestellte Fragen', 
+                icon: 'pi pi-fw pi-question-circle', 
+                questions: [
+                    'Was ist Quivis?', 
+                    'Wie funktioniert Quivis?'
                 ] 
             },
-            { 
-                label: 'Support', icon: 'pi pi-fw pi-question-circle', questions: [
-                    'How can I get support?', 'What is the response time?', 'Is there a community forum?', 'Is live chat available?'
-                ] 
-            },
-            { 
-                label: 'Billing', icon: 'pi pi-fw pi-credit-card', questions: [
-                    'Will I receive an invoice?', 'How to provide my billing information?', 'Is VAT included?', 'Can I receive PDF invoices?'
-                ] 
-            }
         ];
+
+        this.answers = [
+            {
+                'Dashboard': 'Das Dashboard ist die Startseite von Quivis. Hier finden Sie alle wichtigen Informationen auf einen Blick. Sie können Ihre Medikamente eintragen, Ihre Medizinische Historie einsehen und mit Claude chatten.',
+                'Medizinische Historie': 'In der Medizinischen Historie können Sie alle Ihre Arztbesuche, Diagnosen und Behandlungen eintragen. So haben Sie alle wichtigen Informationen immer griffbereit.',
+                'Medikationsplan': 'Im Medikationsplan können Sie alle Ihre Medikamente eintragen. Sie können die Dosierung, die Einnahmezeit und die Dauer der Einnahme festlegen. So haben Sie immer im Blick, wann Sie welche Medikamente einnehmen müssen.',
+                'Frag Claude': 'Claude ist Ihr persönlicher Gesundheitsassistent. Sie können ihm alle Fragen rund um Ihre Gesundheit stellen und er wird Ihnen mit Rat und Tat zur Seite stehen.'
+            },
+            {
+                'Was ist Quivis?': 'Quivis ist eine Plattform, um deine Gesundheitsdaten zu speichern und zu verwalten. Mit Quivis kannst du deine medizinische Historie in das Sprachmodell Claude einlesen und als ergänzenden Ideengeber verwenden, um dein Verständnis zu vertiefen und deine Gesprächsgrundlage mit Ärzten zu erweitern.',
+                'Wie funktioniert Quivis?': 'Quivis ist eine webbasierte Plattform. Um Quivis nutzen zu können, kannst du dir einen Account erstellen und dich anschließend über die Loginseite einloggen. Nach dem Einloggen kannst du Medikationspläne per Foto einlesen, deine Medizinische Historie erfassen und mit dem Sprachmodell Claude chatten.'
+            },
+        ]
     };
+
+    
 
     changeItem(i: number) {
         this.activeIndex = i;
