261b695b5f20641d09ca2483c1cbfcceba8742d2
Tobias Kroell <kroelltob@gmail.com>
2024-05-22 18:38:52 +0200

    Add initial spring files

---
+++ quivis-app/backend/src/main/java/com/quivis/quivis/QuivisApplication.java
@@ -0,0 +1,12 @@
+package com.quivis.quivis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuivisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QuivisApplication.class, args);
+    }
+
+}
