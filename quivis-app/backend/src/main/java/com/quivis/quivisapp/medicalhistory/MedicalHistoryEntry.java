966a675a5a7f0bdbde85e41e393f83646b8a9be4
Tobias Kroell <kroelltob@gmail.com>
2024-05-25 11:57:27 +0200

    Add getters and setters

--- quivis-app/backend/src/main/java/com/quivis/quivisapp/medicalhistory/MedicalHistoryEntry.java
+++ quivis-app/backend/src/main/java/com/quivis/quivisapp/medicalhistory/MedicalHistoryEntry.java
@@ -13,4 +13,36 @@ public class MedicalHistoryEntry {
     private String title;
     private String date;
     private String text;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
