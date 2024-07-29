8abea24d7fd483b6d610f0bae88d3e0267fc350a
Tobias Kroell <kroelltob@gmail.com>
2024-07-29 20:27:36 +0200

    Adjust DDL

--- quivis-app/backend/database_assets/ddl.sql
+++ quivis-app/backend/database_assets/ddl.sql
@@ -1,25 +1,25 @@
-CREATE TABLE PATIENT_DATA (
-    id SERIAL PRIMARY KEY,
+CREATE TABLE USER_DATA (
+    user_id SERIAL PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     date_of_birth DATE NOT NULL,
-	weight INTEGER,
-	gender TEXT,
-	allergies TEXT,
-    print_date DATE
+    weight INTEGER,
+    gender TEXT,
+    allergies TEXT,
+	claude_api_key TEXT
 );
 
 CREATE TABLE MEDICAL_HISTORY (
-    id SERIAL PRIMARY KEY,
-    patient_id INTEGER NOT NULL,
+    medical_history_id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
     title VARCHAR(100) NOT NULL,
     date DATE NOT NULL,
     text TEXT,
-    FOREIGN KEY (patient_id) REFERENCES PATIENT_DATA(id)
+    FOREIGN KEY (user_id) REFERENCES USER_DATA(user_id)
 );
 
 CREATE TABLE ISSUING_DOCTORS_OFFICE (
-    id SERIAL PRIMARY KEY,
+    issuing_doctors_office_id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     street VARCHAR(100) NOT NULL,
     house_number VARCHAR(20) NOT NULL,
@@ -30,28 +30,29 @@ CREATE TABLE ISSUING_DOCTORS_OFFICE (
 );
 
 CREATE TABLE DRUG (
-    id SERIAL PRIMARY KEY,
+    drug_id SERIAL PRIMARY KEY,
     agent VARCHAR(100) NOT NULL,
     brand VARCHAR(100),
     potency VARCHAR(50),
     form VARCHAR(50),
-	unit VARCHAR(20)
+    unit VARCHAR(20)
 );
 
 CREATE TABLE MEDICATION_PLAN (
-    id SERIAL PRIMARY KEY,
+    medication_plan_id SERIAL PRIMARY KEY,
     patient_id INTEGER NOT NULL,
     drug_id INTEGER NOT NULL,
     issued_by INTEGER NOT NULL,
     issue_date DATE NOT NULL,
-	time_morning FLOAT,
+    time_morning FLOAT,
     time_noon FLOAT,
     time_evening FLOAT,
     time_night FLOAT,
-	time_other TEXT,
+    time_other TEXT,
     indications TEXT,
     reason TEXT,
-    FOREIGN KEY (patient_id) REFERENCES PATIENT_DATA(id),
-    FOREIGN KEY (drug_id) REFERENCES DRUG(id),
-    FOREIGN KEY (issued_by) REFERENCES ISSUING_DOCTORS_OFFICE(id)
+	print_date DATE
+    FOREIGN KEY (patient_id) REFERENCES USER_DATA(user_id),
+    FOREIGN KEY (drug_id) REFERENCES DRUG(drug_id),
+    FOREIGN KEY (issued_by) REFERENCES ISSUING_DOCTORS_OFFICE(issuing_doctors_office_id)
 );
